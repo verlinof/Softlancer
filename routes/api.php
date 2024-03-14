d379c6ae2c2f0244c5cb084bca1156b5647aa4b8
verlinof <ajikoko.zx@gmail.com>
2024-03-14 22:31:03 +0700

    FIx API: <Api.php code>

--- routes/api.php
+++ routes/api.php
@@ -8,32 +8,40 @@ use App\Http\Controllers\api\ProjectRoleController;
 use App\Http\Controllers\api\RefferenceController;
 use App\Http\Controllers\api\RoleController;
 use Illuminate\Support\Facades\Route;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-//Login API
+//Authentication API
 Route::get('/auth/google/redirect', [AuthController::class, 'redirectGoogle']);
 Route::get('/auth/google/callback', [AuthController::class, 'googleCallback']);
 //Projects API
 Route::get("/projects", [ProjectController::class, "index"]);
 Route::get("/projects/{id}", [ProjectController::class, "show"]);
-//Roles API
-Route::get("/roles", [RoleController::class, "index"]);
-Route::post("/roles", [RoleController::class, "store"])->middleware(['auth:sanctum', 'AdminAccess']);
-//ProjectRole API
-Route::get("/project-roles", [ProjectRoleController::class, "show"]);
+Route::middleware(['auth:sanctum', 'AdminAccess'])->group(function () {
+  Route::post("/projects", [ProjectController::class, "store"]);
+  Route::patch("/projects/{id}", [ProjectController::class, "update"]);
+  Route::delete("/projects/{id}", [ProjectController::class, "destroy"]);
+  Route::get("/projects/close-project/{id}", [ProjectController::class, "closeProject"]);
+  Route::get("/projects/open-project/{id}", [ProjectController::class, "openProject"]);
+});
+//Application API
+Route::middleware(['auth:sanctum'])->group(function () {
+  //Middleware Application Owner check user->id == application->user_id
+  Route::middleware(['ApplicationOwner'])->group(function () {
+    Route::patch("/applications/{id}", [ApplicationController::class, "update"]);
+    Route::delete("/applications/{id}", [ApplicationController::class, "destroy"]);
+  });
+  Route::get("/applications/handle/{id}", [ApplicationController::class, "handleApplication"])->middleware(['AdminAccess']); //Untuk handle application
+  Route::post("/applications", [ApplicationController::class, "store"]);
+  Route::get("/applications", [ApplicationController::class, "show"]);
+});
 //Company API
 Route::get("/company", [CompanyController::class, "index"]);
 Route::get("/company/{id}", [CompanyController::class, "show"]);
-//Sanctum
+Route::middleware(['auth:sanctum', 'AdminAccess'])->group(function () {
+  Route::post("/company", [CompanyController::class, "store"]);
+  Route::patch("/company/{id}", [CompanyController::class, "update"]);
+  Route::delete("/company/{id}", [CompanyController::class, "destroy"]);
+});
+//User API and Refferences API
 Route::middleware(['auth:sanctum'])->group(function () {
   //User API
   Route::get("/profile", [AuthController::class, "profile"]);
@@ -44,27 +52,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
   Route::post("/refferences", [RefferenceController::class, "store"]);
   Route::get("/profile-refferences", [RefferenceController::class, "showByUser"]);
   Route::delete("/refferences/{id}", [RefferenceController::class, "destroy"]);
-  //Middleware Admin  
-  Route::middleware(['AdminAccess'])->group(function () {
-    //Projects API
-    Route::post("/projects", [ProjectController::class, "store"]);
-    Route::patch("/projects/{id}", [ProjectController::class, "update"]);
-    Route::delete("/projects/{id}", [ProjectController::class, "destroy"]);
-    Route::get("/projects/close-project/{id}", [ProjectController::class, "closeProject"]);
-    Route::get("/projects/open-project/{id}", [ProjectController::class, "openProject"]);
-    //Company API
-    Route::post("/company", [CompanyController::class, "store"]);
-    Route::patch("/company/{id}", [CompanyController::class, "update"]);
-    Route::delete("/company/{id}", [CompanyController::class, "destroy"]);
-    //Application API
-    Route::get("/applications/handle/{id}", [ApplicationController::class, "handleApplication"]); //Untuk handle application
-  });
-  //Middleware Application Owner check user->id == application->user_id
-  //Application API
-  Route::middleware(['ApplicationOwner'])->group(function () {
-    Route::patch("/applications/{id}", [ApplicationController::class, "update"]);
-    Route::delete("/applications/{id}", [ApplicationController::class, "destroy"]);
-  });
-  Route::post("/applications", [ApplicationController::class, "store"]);
-  Route::get("/applications", [ApplicationController::class, "show"]);
 });
+//Roles API
+Route::get("/roles", [RoleController::class, "index"]);
+Route::post("/roles", [RoleController::class, "store"])->middleware(['auth:sanctum', 'AdminAccess']);
+//ProjectRole API
+Route::get("/project-roles", [ProjectRoleController::class, "show"]);
